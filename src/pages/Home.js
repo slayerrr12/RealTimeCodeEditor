465176b80c6692bc90b9bf7a24efea7ca0b78015
slayerrr12 <eyeamarinsharma@gmail.com>
2023-08-09 01:07:18 +0530

    footer structure setup

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,20 +3,31 @@ import React from 'react'
 
 const Home = () => {
     return (
-        <div className='homePageWrapper'>
-            <div className='formWrapper'>
+        <>
+            <div className='homePageWrapper'>
+                <div className='formWrapper'>
 
-                <h2>Stream Script</h2>
-                <h4 className='mainLabel'> Paste Room Id</h4>
-                <div>
-                    <input type="text" className='inputbox' placeholder='RoomId' />
-                    <input type="text" className='inputbox' placeholder='UserName' />
+                    <h2>Stream Script</h2>
+                    <h4 className='mainLabel'> Paste Room Id</h4>
+                    <div>
+                        <input type="text" className='inputbox' placeholder='RoomId' />
+                        <input type="text" className='inputbox' placeholder='UserName' />
+                    </div>
+                    <button className='btn joinBtn'>Join</button>
+                    <span>if you Dont have an invote then create </span>
+                    <a href='/' className='createNewBtn'>New Room</a>
                 </div>
-                <button className='btn joinBtn'>Join</button>
-                <span>if you Dont have an invote then create a room</span>
-            </div>
 
-        </div>
+            </div>
+            <footer>
+                <h4>
+                    Made By <a href='https://github.com/slayerrr12'>
+                        @slayerrr12
+                    </a>
+                </h4>
+            </footer>
+        </>
+
     )
 }
 
