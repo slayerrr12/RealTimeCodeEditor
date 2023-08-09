484ba7eb664a9cc0480a7431a3c9796acc6856bb
slayerrr12 <eyeamarinsharma@gmail.com>
2023-08-10 02:14:22 +0530

    configured toaster for joinroom button

--- src/App.js
+++ src/App.js
@@ -1,29 +1,57 @@
 
 import './App.css';
-import {BrowserRouter , Routes , Route, Router} from 'react-router-dom' 
+import { BrowserRouter, Routes, Route, Router } from 'react-router-dom'
 
 
 //pages imports
-import  Editor  from './pages/Editor';
-import  Home  from './pages/Home';
+import Editor from './pages/Editor';
+import Home from './pages/Home';
+import { Toaster } from 'react-hot-toast';
 
 function App() {
   return (
-   
+
+
+    <>
+
+      <div>
+        <Toaster
+
+          toastOptions={
+            {
+              success: {
+                theme: {
+                  primary: '#10b981',
+                }
+              }
+            }
+          }
+          position="top-center"
+          reverseOrder={false}
+        />
+      </div>
+
+
+
+
+
       <BrowserRouter>
 
-      <Routes>
-        <Route path='/' element={<Home/>} ></Route>
-        <Route path='/editor/:roomId' element={<Editor/>}></Route>
+        <Routes>
+          <Route path='/' element={<Home />} ></Route>
+          <Route path='/editor/:roomId' element={<Editor />}></Route>
 
 
-      
 
-      </Routes>
+
+        </Routes>
 
 
       </BrowserRouter>
-    
+
+    </>
+
+
   );
 }
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,156 +1,159 @@
 import React from "react";
 import { useState } from "react";
-import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4 } from "uuid";
+import toast from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
-    const [roomId, setRoomId] = useState('')
-    const [username, setUserName] = useState('')
+    const navigate = useNavigate();
+    const [roomId, setRoomId] = useState("");
+    const [userName, setUserName] = useState("");
 
     const createRoom = (e) => {
         e.preventDefault();
 
-        const id = uuidv4()
-        setRoomId(id)
+        const id = uuidv4();
+        setRoomId(id);
 
-    }
+        toast.success("Created New Room");
+    };
 
-
-
-
-
-
-
-
-
+    const joinRoom = () => {
+        if (!roomId || !userName) {
+            toast.error("Room ID and username is required");
+            return;
+        }
+        //redirect
+        navigate(`/editor/${roomId}`, {
+            state: { username: userName },
+        });
+    };
     return (
-        
-            <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-                <a
-                    href="#m"
-                    class="flex items-center mb-6 text-rose-800 text-2xl font-semibold "
-                >
-                    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
-                    StreamScript
-                </a>
-                <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
-                    <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
-                        <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-                            Enter Room ID
-                        </h1>
-                        <form class="space-y-4 md:space-y-6" action="#">
-                            <div>
-                                <label
-                                    for="text"
-                                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                                >
-                                    Your RoomID
-                                </label>
-                                <input
+        <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+            <a
+                href="#m"
+                class="flex items-center mb-6 text-rose-800 text-2xl font-semibold "
+            >
+                <link
+                    rel="stylesheet"
+                    href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
+                />
+                StreamScript
+            </a>
+            <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+                <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
+                    <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                        Enter Room ID
+                    </h1>
+                    <form class="space-y-4 md:space-y-6" action="#">
+                        <div>
+                            <label
+                                for="text"
+                                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                            >
+                                Your RoomID
+                            </label>
+                            <input
+                                name="roomId"
+                                id="roomId"
+                                value={roomId}
+                                onChange={(e) => setRoomId(e.target.value)}
+                                class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                            />
+                        </div>
+                        <div>
+                            <label
+                                for="username"
+                                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                            >
+                                Username
+                            </label>
+                            <input
+                                type="text"
+                                name="username"
+                                id="username"
+                                placeholder="Username"
+                                onChange={(e) => setUserName(e.target.value)}
+                                class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                required=""
+                            />
+                        </div>
+                        <div class="flex items-center justify-between"></div>
+                        <div className="w-full text-center">
+                            <button
+                                onClick={() => joinRoom()}
+                                className="relative inline-flex items-center justify-center p-4 px-6 py-3 overflow-hidden font-medium text-indigo-600 transition duration-300 ease-out border-2 border-purple-500 rounded-full shadow-md group"
+                            >
+                                <span class="absolute inset-0 flex items-center justify-center w-full h-full text-white duration-300 -translate-x-full bg-purple-500 group-hover:translate-x-0 ease">
+                                    <svg
+                                        class="w-6 h-6"
+                                        fill="none"
+                                        stroke="currentColor"
+                                        viewBox="0 0 24 24"
+                                        xmlns="http://www.w3.org/2000/svg"
+                                    >
+                                        <path
+                                            stroke-linecap="round"
+                                            stroke-linejoin="round"
+                                            stroke-width="2"
+                                            d="M14 5l7 7m0 0l-7 7m7-7H3"
+                                        ></path>
+                                    </svg>
+                                </span>
+                                <span class="absolute flex items-center justify-center w-full h-full text-purple-500 transition-all duration-300 transform group-hover:translate-x-full ease">
+                                    JOIN ROOM
+                                </span>
+                                <span class="relative invisible">JOIN ROOM</span>
+                            </button>
+                        </div>
 
-                                    name="roomId"
-                                    id="roomId"
-                                    value={roomId}
-                                    onChange={(e) => setRoomId(e.target.value)}
-                                    class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                />
-                            </div>
-                            <div>
-                                <label
-                                    for="username"
-                                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                                >
-                                    Username
-                                </label>
-                                <input
-                                    type="text"
-                                    name="username"
-                                    id="username"
-                                    placeholder="Username"
-                                    onChange={(e) => setUserName(e.target.value)}
-                                    class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                    required=""
-                                />
-                            </div>
-                            <div class="flex items-center justify-between"></div>
-                            <div className="w-full text-center">
-                                <a
-                                    href="#_"
-                                    class="relative inline-flex items-center justify-center p-4 px-6 py-3 overflow-hidden font-medium text-indigo-600 transition duration-300 ease-out border-2 border-purple-500 rounded-full shadow-md group"
-                                >
-                                    <span class="absolute inset-0 flex items-center justify-center w-full h-full text-white duration-300 -translate-x-full bg-purple-500 group-hover:translate-x-0 ease">
-                                        <svg
-                                            class="w-6 h-6"
-                                            fill="none"
-                                            stroke="currentColor"
-                                            viewBox="0 0 24 24"
-                                            xmlns="http://www.w3.org/2000/svg"
-                                        >
-                                            <path
-                                                stroke-linecap="round"
-                                                stroke-linejoin="round"
-                                                stroke-width="2"
-                                                d="M14 5l7 7m0 0l-7 7m7-7H3"
-                                            ></path>
-                                        </svg>
-                                    </span>
-                                    <span class="absolute flex items-center justify-center w-full h-full text-purple-500 transition-all duration-300 transform group-hover:translate-x-full ease">
-                                        JOIN ROOM
-                                    </span>
-                                    <span class="relative invisible">JOIN ROOM</span>
-                                </a>
-                            </div>
-
-                            <div className="w-full text-center">
-
-                                <button
-                                    onClick={createRoom}
-                                    class="relative inline-flex items-center justify-start py-3 pl-4 pr-12 overflow-hidden font-semibold text-indigo-600 transition-all duration-150 ease-in-out rounded hover:pl-10 hover:pr-6 bg-gray-50 group"
-                                >
-                                    <span class="absolute bottom-0 left-0 w-full h-1 transition-all duration-150 ease-in-out bg-indigo-600 group-hover:h-full"></span>
-                                    <span class="absolute right-0 pr-4 duration-200 ease-out group-hover:translate-x-12">
-                                        <svg
-                                            class="w-5 h-5 text-green-400"
-                                            fill="none"
-                                            stroke="currentColor"
-                                            viewBox="0 0 24 24"
-                                            xmlns="http://www.w3.org/2000/svg"
-                                        >
-                                            <path
-                                                stroke-linecap="round"
-                                                stroke-linejoin="round"
-                                                stroke-width="2"
-                                                d="M14 5l7 7m0 0l-7 7m7-7H3"
-                                            ></path>
-                                        </svg>
-                                    </span>
-                                    <span class="absolute left-0 pl-2.5 -translate-x-12 group-hover:translate-x-0 ease-out duration-200">
-                                        <svg
-                                            class="w-5 h-5 text-green-400"
-                                            fill="none"
-                                            stroke="currentColor"
-                                            viewBox="0 0 24 24"
-                                            xmlns="http://www.w3.org/2000/svg"
-                                        >
-                                            <path
-                                                stroke-linecap="round"
-                                                stroke-linejoin="round"
-                                                stroke-width="2"
-                                                d="M14 5l7 7m0 0l-7 7m7-7H3"
-                                            ></path>
-                                        </svg>
-                                    </span>
-                                    <span class="relative w-full text-left transition-colors duration-200 ease-in-out group-hover:text-white">
-                                        Create A Room
-                                    </span>
-                                </button>
-
-
-                            </div>
-                        </form>
-                    </div>
+                        <div className="w-full text-center">
+                            <button
+                                onClick={createRoom}
+                                class="relative inline-flex items-center justify-start py-3 pl-4 pr-12 overflow-hidden font-semibold text-indigo-600 transition-all duration-150 ease-in-out rounded hover:pl-10 hover:pr-6 bg-gray-50 group"
+                            >
+                                <span class="absolute bottom-0 left-0 w-full h-1 transition-all duration-150 ease-in-out bg-indigo-600 group-hover:h-full"></span>
+                                <span class="absolute right-0 pr-4 duration-200 ease-out group-hover:translate-x-12">
+                                    <svg
+                                        class="w-5 h-5 text-green-400"
+                                        fill="none"
+                                        stroke="currentColor"
+                                        viewBox="0 0 24 24"
+                                        xmlns="http://www.w3.org/2000/svg"
+                                    >
+                                        <path
+                                            stroke-linecap="round"
+                                            stroke-linejoin="round"
+                                            stroke-width="2"
+                                            d="M14 5l7 7m0 0l-7 7m7-7H3"
+                                        ></path>
+                                    </svg>
+                                </span>
+                                <span class="absolute left-0 pl-2.5 -translate-x-12 group-hover:translate-x-0 ease-out duration-200">
+                                    <svg
+                                        class="w-5 h-5 text-green-400"
+                                        fill="none"
+                                        stroke="currentColor"
+                                        viewBox="0 0 24 24"
+                                        xmlns="http://www.w3.org/2000/svg"
+                                    >
+                                        <path
+                                            stroke-linecap="round"
+                                            stroke-linejoin="round"
+                                            stroke-width="2"
+                                            d="M14 5l7 7m0 0l-7 7m7-7H3"
+                                        ></path>
+                                    </svg>
+                                </span>
+                                <span class="relative w-full text-left transition-colors duration-200 ease-in-out group-hover:text-white">
+                                    Create A Room
+                                </span>
+                            </button>
+                        </div>
+                    </form>
                 </div>
             </div>
-       
+        </div>
     );
 };
 
