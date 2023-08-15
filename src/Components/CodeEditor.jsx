a79cf93e75cfde70939ade78a114bca9d479467e
slayerrr12 <eyeamarinsharma@gmail.com>
2023-08-15 21:05:57 +0530

    install codemirror  v5 and uninstalled v6

--- src/Components/CodeEditor.jsx
+++ src/Components/CodeEditor.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import CodeMirror from 'codemirror'
 
 const CodeEditor = () => {
-  return (
-    <div>
-      editor goes here
-    </div>
-  )
+
+    useEffect(() => {
+        async function init() {
+            CodeMirror.fromTextArea(document.getElementById('realtimeEditor'), {
+                mode: {
+                    name: "javascript"
+                    ,
+                    json: true
+                }
+            })
+
+        }
+        init()
+    })
+
+
+    return (
+        <textarea id='realtimeEditor'></textarea>
+    )
 }
 
 export default CodeEditor
