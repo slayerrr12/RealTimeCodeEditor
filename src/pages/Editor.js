e8d19648200a4241846c212f9a795c186c6dc5a9
slayerrr12 <eyeamarinsharma@gmail.com>
2023-08-16 11:16:31 +0530

    added socket , socket.io express packages

--- src/pages/Editor.js
+++ src/pages/Editor.js
@@ -12,38 +12,32 @@ const Editor = () => {
     <div className="relative">
       <nav class="bg-white sticky top-0 z-50	 w-full border-gray-200 dark:bg-gray-900">
         <div class=" flex flex-wrap items-center justify-between mx-auto p-4">
-          <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-            StreamScript
-          </span>
+          <a href="/">
+            <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+              StreamScript
+            </span>
+          </a>
         </div>
       </nav>
 
       <div className="mainWrap">
-            <div className="aside">
-                <div className="asideInner">
-                    
-                    <h3>Users In This Room</h3>
-                    <div className="clientsList mt-5">
-                        {clients.map((client) => (
-                            <Client
-                                key={client.socketId}
-                                username={client.username}
-                            />
-                        ))}
-                    </div>
-                </div>
-                <button>
-                    Copy Room ID
-                </button>
-                <button>
-                  Leave Room
-                </button>
+        <div className="aside">
+          <div className="asideInner">
+            <h3>Users In This Room</h3>
+            <div className="clientsList mt-5">
+              {clients.map((client) => (
+                <Client key={client.socketId} username={client.username} />
+              ))}
             </div>
-            <div className="editorWrap">
-                <CodeEditor/>
-            </div>
+          </div>
+          <button>Copy Room ID</button>
+          <button>Leave Room</button>
         </div>
+        <div className="editorWrap">
+          <CodeEditor />
         </div>
+      </div>
+    </div>
   );
 };
 
